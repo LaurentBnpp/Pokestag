268c55945a66d6f10984319f16f7b0ab10ebecc4
Laurent Jeanjean <laurent.jeanjean@bnpparibas.com>
2023-07-04 14:58:37 +0200

    feat: Project creation

---
+++ PokeStag/PokeStag/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PokeStag
+//
+//  Created by Laurent Jeanjean on 04/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeStag/PokeStag/PokeStagApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeStagApp.swift
+//  PokeStag
+//
+//  Created by Laurent Jeanjean on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokeStagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
